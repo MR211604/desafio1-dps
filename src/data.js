45cd130ce751c462d55e4e7d39e729d8b28f5e22
MR211604 <elmastrollxd34@gmail.com>
2025-07-26 09:59:21 -0600

    initial commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const data=[
+{
+id:1,
+title:'Mouse Razer Viper V3',
+price:10,
+urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada65cfa79b5fa36.jpg',
+quantity:1
+},
+{
+id:2,
+  title: 'Teclado Razer Blackwidow V3',
+price:30,
+urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada64751edc681ea.jpg',
+quantity:1
+},
+{
+id:3,
+title:'Hyperx Cloud Stinger Core 2',
+price:30,
+urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada65a170ce041a7.jpg',
+quantity:1
+},
+{
+id:4,
+title:'Monitor samsung 27"',
+price:30,
+urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada684853cbd1263.jpg',
+quantity:1
+}, {
+id:5,
+title:'UPS Forza2400VA',
+price:199.95,
+urlImage:'https://apizd.zonadigitalsv.com/storage/productos/cWfylwIR3ClBJ76DwGT1NBt8eE41TEIaYGVc92Oe.png',
+quantity:1
+}
+];
